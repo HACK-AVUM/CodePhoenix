06788ec830cd994b880195a7c9d668f3fb8e365f
val-2 <programming.recantingly@aleeas.com>
2024-10-09 23:20:54 +0200

    Update vite.config.ts

--- frontend_react/vite.config.ts
+++ frontend_react/vite.config.ts
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Allow access from the outside
     port: 3000,
-    https: true, // Use HTTPS
-    hmr: {
-      protocol: 'wss', // Use WebSocket Secure
-      host: 'codephoenix.italynorth.cloudapp.azure.com', // Set the correct host
-    },
+    host: true,
   },
-});
+})
